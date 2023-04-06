8d29430ffbb187cf780c16ba452790f769f296e4
TechieDevice <avenger.148@gmail.com>
2023-04-06 17:54:17 +0700

    add empty-data filter, mne

--- EEG_testing/eeg_receiver.py
+++ EEG_testing/eeg_receiver.py
@@ -16,7 +16,9 @@ def grab_data(stop_event):
     while True:
         r = requests.get('http://127.0.0.1:2336/grabRawData')
         json_r = json.loads(r.text)
-        data = json_r["data"]
-        print(data)
+        new_data = json_r["data"]
+        if len(new_data[0]) == 0:
+            continue
+
         if stop_event.is_set():
             break
--- EEG_testing/testing_main.py
+++ EEG_testing/testing_main.py
@@ -3,6 +3,8 @@ import PySimpleGUI as sg
 import numpy as np
 import testing_disp
 
+import mne
+
 def disp_window(window):
     window.Hide()
     testing_disp.main()
@@ -26,6 +28,10 @@ layout = [
 
 def main():
     window = sg.Window('Window', layout, size=(550,300))
+    
+    #file = "C:\\Users\Ilya Kovalev\\Documents\\NeuroPlayPro\\2023.04.06-16.45.43.600.edf"
+    #data = mne.io.read_raw_edf(file)
+    #raw_data = data.get_data()
 
     while True:
         event, values = window.Read()
